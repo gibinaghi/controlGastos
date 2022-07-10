4b6a686855548041904c808cd33d477338bb1fa8
Gimena Binaghi <gimenabinaghi@hotmail.com>
2022-07-10 16:23:23 -0300

    primer comentario

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'> 
+            <form className='formulario'>
+                <div className='campo'>
+                    <label> Definir presupuesto </label>
+                    <input 
+                    className='nuevo-presupuesto'
+                    type='text'
+                    placeholder='Añade tu presupuesto'
+                    value={presupuesto}
+                    onChange= {(e) => setPresupuesto(e.target.value) }
+                    />
+                </div>
+                <input type='submit' value='Añadir' />
+            </form>
+        </div>
+    )
+}
+
+export default NuevoPresupuesto
